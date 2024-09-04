2052ce4a6b5fb3193f696ce87ac1a9e3c9bf1213
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-09-04 14:00:54 +0200

    implementation of UnitOfWork

--- EvidencijaRadnogVremena/Data/Repositories/Interfaces/IUnitOfWork.cs
+++ EvidencijaRadnogVremena/Data/Repositories/Interfaces/IUnitOfWork.cs
@@ -1,8 +1,14 @@
 ﻿namespace EvidencijaRadnogVremena.Data.Repositories.Interfaces
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        //Dodati repozitorije
+        // IGenericRepository<Actor> Actors {get;}
+        //...
+
         Task CompleteAsync();
+        Task CommitAsync();
+        Task RollbackAsync();
 
     }
 }
--- EvidencijaRadnogVremena/Data/Repositories/UnitOfWork.cs
+++ EvidencijaRadnogVremena/Data/Repositories/UnitOfWork.cs
@@ -15,5 +15,26 @@ namespace EvidencijaRadnogVremena.Data.Repositories
         {
             await _context.SaveChangesAsync();
         }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        Task IUnitOfWork.CommitAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IUnitOfWork.RollbackAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        //private IGenericRepository<Actor> _actors;
+
+        //public IGenericRepository<Actor> Actors => _actors ??= new GenericRepository<Actor>(_context);
+        //Generički repo je onaj sa standardim CRUD operacijama prema bazi, ovdje proslijeđujemo isti _context svima te unit of work odrađuje posao...
     }
 }
